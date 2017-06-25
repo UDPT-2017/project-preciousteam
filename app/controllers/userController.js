f194a041957c8bc983c45943d6b70429ed63177e
thikhin96 <nghoangthi1@gmail.com>
2017-06-25 22:22:36 +0700

    update user info

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,5 +1,8 @@
 const Cart = require('../models/cart')
 const Product = require('../models/product')
+const multer = require('multer')
+const storage = require('../../config/upload')
+const User = require('../models/users')
 
 
 const userController  = {
@@ -54,6 +57,47 @@ const userController  = {
 		res.render('createpost', {
 			user: req.user
 		});
+	},
+	updateUser: function(req, res){
+		const upload = multer({ storage : storage}).single('userPhoto');
+		upload(req,res,function(err) {
+			if (err){
+				console.log(err);
+			}
+			else
+			{
+				console.log(req.body);
+			if (req.body.changePic == 1){
+				User.update(req.body.tel, req.body.pass, req.file.filename, req.user.userid, function(err1, ress){
+					if (err1 != null)
+					{
+						console.log(err1);
+						res.end('0');
+					}
+					else
+					{
+						res.end('1');
+					}
+				})				
+			}
+			else
+			{
+				User.update(req.body.tel, req.body.pass, null, req.user.userid, function(err1, ress){
+					if (err1 != null)
+					{
+						console.log(err1);
+						res.end('0');
+					}
+					else
+					{
+						res.end('1');
+					}
+				})
+			}
+			}
+			
+		})
+		
 	}
 };
 
